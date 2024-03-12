bfacc21b9a4b8e1343882633bf9214a4a2cb12e3
venkatasaisabbineni <61623131+venkatasaisabbineni@users.noreply.github.com>
2024-03-12 18:03:53 -0400

    Update sparkLambdaHandler.py

--- sparkLambdaHandler.py
+++ sparkLambdaHandler.py
@@ -41,16 +41,7 @@ def spark_submit(s3_bucket_script: str,input_script: str, event: dict)-> None:
 
     #logger.info(f'Trigger File Name : {event['Records']}')
     for key,value in event.items():
-        a = value[0]
-    for key,value in a.items():
-        b = a['s3']
-    for key,value in b.items():
-        c = b['object']
-    for key,value in c.items():
-        d = c['key']
-    logger.info(f'Trigger File Name : {d}')
-    # for key,value in event.items():
-    #     logger.info(f'Key of the event : {key}, Value of the event : {value}')
+         logger.info(f'Key of the event : {key}, Value of the event : {value}')
     # Run the spark-submit command on the local copy of teh script
     try:
         logger.info(f'Spark-Submitting the Spark script {input_script} from {s3_bucket_script}')
